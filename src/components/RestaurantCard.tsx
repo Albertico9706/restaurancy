7a1159e569e098a1eb44103f03ea6dc161fd655d
Alberto <abcelkuqui@gmail.com>
2023-12-27 20:09:13 -0500

    i think that is fine

--- src/components/RestaurantCard.tsx
+++ src/components/RestaurantCard.tsx
@@ -17,7 +17,7 @@ export default function RestaurantCard({rest}:{rest:Restaurant}){
             <Link className="" href={`/${rest.id}`}>
                 <h2 className="text-3xl border-b border-b-transparent group-hover:text-blue-700  group-hover:border-b-blue-700 dark:group-hover:text-orange-500  dark:group-hover:border-b-orange-500 transition-all">{rest.name}</h2>
             </Link>
-            <div className="grid  h-[30%] bg-violet-800 bg-opacity-5 justify-self-center place-content-center" >
+            <div className="grid min-h-[160px]  h-[30%] bg-violet-800 bg-opacity-5 justify-self-center place-content-center" >
                 <img className="h-full transition-all " src={rest.image} alt="Imagen del Restaurant" />
             </div>
             <p className="opacity-90">{rest.description}</p>
