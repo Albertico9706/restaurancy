c8961ad693fdb86ff9a481181f62b046e9d95a23
Albertico9706 <abcelqui@gmail.com>
2024-05-25 18:11:57 -0500

    begin whit styles in navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,9 +3,9 @@ import { ButtonTheme } from '@/components/ButtonTheme'
 
 export default function NavBar(){
     return(
-        <nav className="flex justify-between p-4 bg-orange-600/40 fixed top-0 left-0 right-0 z-40
-        rounded-b-lg pe-16 ">
-            <a href="#">Restaurancy</a>
+        <nav className="flex justify-between p-4 bg-orange-600/40 fixed top-0 left-0 right-0 z-40 bg-slate-600 border-b-2 border-orange-600 
+         pe-16 ">
+            <a href="#" className='text-3xl text-orange-500  font-weight-700'>Restaurancy</a>
 
             <ul className="flex gap-2">
                 
